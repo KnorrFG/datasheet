7dfae2a7b0088db3cc81a865deceb7090b7c48a2
Felix Knorr <knorr.felix@gmx.de>
2024-10-09 10:43:37 +0200

    increase version number

--- datasheet/__init__.py
+++ datasheet/__init__.py
@@ -1,6 +1,6 @@
 """A library to generate HTML pages containing all sorts of output"""
 
-__version__ = "1.2.2"
+__version__ = "1.2.3"
 
 from .sheet import Sheet
 from .types import MD, Str, Repr, DF, Figure, VLayout, HLayout
